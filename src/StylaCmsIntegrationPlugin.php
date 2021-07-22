c70332a87dcc526a4f0202209b30fea7b37fa2ff
Michał Wojtas <michal@michalwojtas.pl>
2021-07-22 16:48:02 +0200

    First release source files

---
+++ src/StylaCmsIntegrationPlugin.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Styla\CmsIntegration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class StylaCmsIntegrationPlugin extends Plugin
+{
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        $this->removeUsedStylaBlocksAndElementsFromThePages();
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        /** @var Connection $connection */
+        $connection = $this->container->get(Connection::class);
+
+        $dropPagesTableSQL = 'DROP TABLE IF EXISTS `styla_cms_page`';
+        $connection->executeStatement($dropPagesTableSQL);
+
+        $dropPagesSynchronizationTableSQL = 'DROP TABLE IF EXISTS `styla_cms_pages_synchronization`';
+        $connection->executeStatement($dropPagesSynchronizationTableSQL);
+    }
+
+    private function removeUsedStylaBlocksAndElementsFromThePages()
+    {
+        /** @var Connection $connection */
+        $connection = $this->container->get(Connection::class);
+
+        $removeStylaBlocks = 'DELETE FROM `cms_block` where type=:stylaBlockType';
+        $connection->executeStatement($removeStylaBlocks, ['stylaBlockType' => 'styla-module-content']);
+
+        $removeStylaElements = 'DELETE FROM `cms_slot` where type=:stylaBlockType';
+        $connection->executeStatement($removeStylaElements, ['stylaBlockType' => 'styla-module-content']);
+    }
+}
